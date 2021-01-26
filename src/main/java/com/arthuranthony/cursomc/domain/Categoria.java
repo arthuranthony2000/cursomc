f5e44ae5e6927e95af45109a0940030bb8b88cc9
Arthur Anthony <arthuranthony2000@gmail.com>
2021-01-26 20:13:58 -0300

    Banco de Dados H2 e criação automática da base de dados

--- src/main/java/com/arthuranthony/cursomc/domain/Categoria.java
+++ src/main/java/com/arthuranthony/cursomc/domain/Categoria.java
@@ -2,10 +2,20 @@ package com.arthuranthony.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
 	public Categoria() {}
 	public Categoria(Integer id, String nome) {
 		super();
@@ -47,8 +57,4 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
 }
